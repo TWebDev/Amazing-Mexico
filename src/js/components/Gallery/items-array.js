87bc5c786caa0a3360864323c828488b11d1769c
Kauyumari <emiliano.jgj97@gmail.com>
2019-08-09 12:08:17 -0500

    Actualización de assets

--- src/js/components/Gallery/items-array.js
+++ src/js/components/Gallery/items-array.js
@@ -1,29 +1,29 @@
-import PV from '../../../media/images/PV.jpg';
-import Cn from '../../../media/images/CN.jpg';
-import Co from '../../../media/images/CO.jpg';
-import Lo from '../../../media/images/LO.jpg';
+import PV from '../../../media/images/PV2.png';
+import Cn from '../../../media/images/CN.png';
+import Co from '../../../media/images/CO.png';
+import Lo from '../../../media/images/LO.png';
 
 
 const array = [
   {
     imgSrc: PV,
     title: "Puerto Vallarta",
-    text: "Visita Puerto Vallarta, no te arrepentirás"
+    text: "Distinguished for a great nightlife and amazing hospitality, so if you are looking for a relaxing time Puerto Vallarta is for you."
   },
   {
     imgSrc: Cn,
     title: "Cancun",
-    text: "Visita Cancun, no te arrepentirás"
+    text: "Breathtaking tropical views and huge cultural heritage Cancun is a place that will blow your mind."
   },
   {
     imgSrc: Co,
-    title: "Cabo",
-    text: "Visita Cabo, no te arrepentirás"
+    title: "Los Cabos",
+    text: "The perfect blend of desert, sea, and adventure. If you are interested in experiencing all kinds of crazy you can never go wrong with Los Cabos."
   },
   {
     imgSrc: Lo,
     title: "Loreto",
-    text: "Visita Loreto, no te arrepentirás"
+    text: "Are you interested in a vacation where you will only have a relaxing with nothing but tranquility and nature Loreto is the one."
   },
 ]
 
